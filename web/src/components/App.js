ff323f207c3632c6f237862095ee02cad79a8547
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-07 11:02:23 +1000

    Update App.js to include CreateLink component

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 // import logo from '../assets/images/logo.svg'
 import '../styles/App.css'
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <div>
+        <LinkList />
+        <CreateLink />
+      </div>
     )
   }
 }
