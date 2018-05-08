cfe46b73836defd559c4c9b382032c81bf18f61d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-05-08 16:44:59 +1000

    Update mutation to require authentication on the server-side

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -2,8 +2,16 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
+// function post(parent, { url, description }, ctx, info) {
+//   return ctx.db.mutation.createLink({ data: { url, description } }, info)
+// }
+// Require authentication on the server-side
 function post(parent, { url, description }, ctx, info) {
-  return ctx.db.mutation.createLink({ data: { url, description } }, info)
+  const userId = getUserId(ctx)
+  return ctx.db.mutation.createLink(
+    { data: { url, description, postedBy: { connect: { id: userId } } } },
+    info,
+  )
 }
 
 async function signup(parent, args, ctx, info) {
